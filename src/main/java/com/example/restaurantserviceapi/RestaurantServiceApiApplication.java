11d718550f06c45c2a1917c15d0cac7bdf0198c4
Leonardo Herrera <leonardo.herrera@fundacion-jala.org>
2024-09-27 13:36:17 -0400

    first commit

---
+++ src/main/java/com/example/restaurantserviceapi/RestaurantServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApiApplication.class, args);
+    }
+
+}
